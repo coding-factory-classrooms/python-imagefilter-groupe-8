d6cd83ae363b642bb21ff437449cb6179465cb7d
Infernaton <lubin.pauly@gmail.com>
2020-12-16 17:11:11 +0100

    Add a help command with -h

--- core.py
+++ core.py
@@ -9,7 +9,7 @@ def modify_img(image):
         image = turn_dilated(image)
         cv2.imshow(f'{image_nbr}', image)
     except cv2.error:
-        print(f"Not a valid file: {image}")
+        print(f"Not a valid file")
 
 # image = cv2.imread('data/montagne.jpg')
 # image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
