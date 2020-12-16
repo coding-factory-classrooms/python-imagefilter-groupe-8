ca9779531246739ae2ce6835b1d8f19758290135
Nathan Lecorbeiller <lecorbeiller.nathan@outlook.fr>
2020-12-16 10:13:23 +0100

    Added basic blur function

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
+import numpy as np
 
-image = cv2.imread('data/dunkey.jpg')
+image = cv2.imread('data/dunkey.jpg') #Reference our image, then set it to gray
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Original image', image)
+cv2.imshow('Original image', image) #Create two images, one gray and one classic
 cv2.imshow('Gray image', gray)
 
-cv2.waitKey(0)
+blur_image = cv2.GaussianBlur(image, (3, 33), 0)
+cv2.imshow('Blurred Image', blur_image)
+
+cv2.waitKey(0) #destroy images when closing program
 cv2.destroyAllWindows()
