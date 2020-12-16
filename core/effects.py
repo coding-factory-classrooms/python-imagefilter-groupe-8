e9a12c8fe4c52b97a26b34b1dfb86782e9ec3069
Nathan Lecorbeiller <lecorbeiller.nathan@outlook.fr>
2020-12-16 11:09:05 +0100

    Created a effet file to store all our possible effect, that we'll call from our main functoin

---
+++ core/effects.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+blur_degree = 9
+
+def turn_gray(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original image', image) #Create two images, one gray and one classic
+    cv2.imshow('Gray image', gray)
+
+
+def turn_blur(image):
+    if blur_degree % 2 !=0:
+        blur_image = cv2.GaussianBlur(image, (blur_degree, blur_degree), blur_degree) #Creating blurred image
+        cv2.imshow('Blurred Image', blur_image)
--- core/main.py
+++ core/main.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
+import effects as ef
 
-image = cv2.imread('data/dunkey.jpg') #Reference our image, then set it to gray
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.imread('data/dunkey.jpg') #Reference our image
 
-cv2.imshow('Original image', image) #Create two images, one gray and one classic
-cv2.imshow('Gray image', gray)
+ef.turn_gray(image)
 
-blur_image = cv2.GaussianBlur(image, (37, 45), 13) #Creating blurred image
-cv2.imshow('Blurred Image', blur_image)
+ef.turn_blur(image)
 
 cv2.waitKey(0) #destroy images when closing program
 cv2.destroyAllWindows()
