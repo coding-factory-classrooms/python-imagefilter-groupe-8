4b81ece1066e754100fdd3b72ad74ba0eab523fd
Infernaton <lubin.pauly@gmail.com>
2020-12-17 10:13:46 +0100

    parameters in filters function

--- effects.py
+++ effects.py
@@ -6,8 +6,7 @@ def turn_gray(image):
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return gray_img
 
-def turn_blur(image):
-    blur_degree = 9
+def turn_blur(image, blur_degree):
 
     if blur_degree % 2 !=0:
         blur_image = cv2.GaussianBlur(image, (blur_degree, blur_degree), blur_degree) #Creating blurred image
@@ -15,7 +14,7 @@ def turn_blur(image):
     else:
         log("Wrong degree given")
 
-def turn_dilated(image):
-    kernel = np.ones((5,5),np.uint8)
+def turn_dilated(image,pixel_value):
+    kernel = np.ones((pixel_value,pixel_value),np.uint8)
     dilation_image = cv2.dilate(image,kernel,iterations = 1 )
     return dilation_image
