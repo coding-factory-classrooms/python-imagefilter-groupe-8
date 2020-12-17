348ef927637997d4ecffe1458d1c2ebf8db17bd4
Infernaton <lubin.pauly@gmail.com>
2020-12-17 11:32:01 +0100

    transform correctly the filters to dict

--- core.py
+++ core.py
@@ -7,36 +7,30 @@ print(args)
 
 image_nbr = 0
 
-def modify_img(image, filters):
-    try:
-        image = turn_gray(image)
-        image = turn_blur(image)
-        image = turn_dilated(image)
-        cv2.imshow(f'{image_nbr}', image)
-    except cv2.error:
-        print(f"Not a valid file")
+def transform_filter(old_filter):
+    filter = old_filter.split(":")
+    print(filter)
+    new_filter = {filter[0]: int(filter[1])}
+    return new_filter
 
-def modify_img(image, filtre):
+def modify_img(image, filter):
     try:
-        for i in range(len(filtre) - 1):
+        for i in range(len(filter) - 1):
             #Filter gray, blur and dilate
-            if filtre[i] == 'gray':
-                filtre = f'{filtre[i + 1]}/'
+            if filter[i] == 'grayscale':
                 image = turn_gray(image)
-            if str(filtre[i]).find('blur')!= -1:
-                filtre = f'{filtre[i + 1]}/'
-                image = turn_blur(image,filtre[i]['blur'])
-            if str(filtre[i]).find('dilate')!= -1:
-                filtre = f'{filtre[i + 1]}/'
-                image = turn_dilated(image,filtre[i]['dilate'])
+            if str(filter[i]).find('blur')!= -1:
+                image = turn_blur(image, filter[i]['blur'])
+            if str(filter[i]).find('dilate')!= -1:
+                image = turn_dilated(image, filter[i]['dilate'])
         cv2.imshow(f'{image_nbr}', image)
 
     except cv2.error:
             print(f"Not a valid file")
 
 
-def save_image(image):
-    image = cv2.imread(image)
-    image_filter = cv2.cvtColor(modify_img(image))
-    cv2.imwrite(f'output/{image_filter}', image_filter)
-
+def save_image(image, output):
+    try:
+        cv2.imwrite(f'{output}', image)
+    except cv2.error:
+            print(f"Not a valid file, failed to save")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from logger import *
 
 try :
-    with open('config.txt','r') as f:
+    with open('confaig.txt','r') as f:
         args = f.read().replace('"','').split()
         print("reading...")
 except FileNotFoundError:
@@ -36,12 +36,16 @@ try:
                 filter_to_apply = args[i + 1].split(
                     "|")  # .split(), split the string into a list with the given separator
 
-                for a in range(len(filter_to_apply) - 1):
+                for a in range(len(filter_to_apply)):
                     # If a blur filter want to be applied, it will change the string to a dict which indicate the blur value
-                    if filter_to_apply[a].find("blur:") != -1 or filter_to_apply[a].find("dilate:") != -1:
-                        filter = filter_to_apply[a].split(":")
-                        new_filter = {filter[0]: int(filter[1])}
-                        filter_to_apply[a] = new_filter
+                    print(filter_to_apply[a])
+
+                    if filter_to_apply[a].find("blur:") != -1:
+                        filter_to_apply[a] = core.transform_filter(filter_to_apply[a])
+
+                    elif filter_to_apply[a].find("dilate:") != -1:
+                        filter_to_apply[a] = core.transform_filter(filter_to_apply[a])
+
                     elif filter_to_apply[a].find("blur") != -1 or filter_to_apply[a].find("dilate") != -1:
                         print(f"You need to define a value for the {filter_to_apply[a]} filter")
                 log(f"FILTERS TO USE - {filter_to_apply}")
@@ -49,9 +53,10 @@ try:
         try: # Because 'entry' and 'filter_to_apply' can not be define, there an except
             with os.scandir(entry) as entries:  # Open the directory as a list
                 for file in entries:  # file represent a picture
+                    path = f'{output}{file}'
                     image = cv2.imread(f'{entry}{file.name}')  # Reference our image
                     core.modify_img(image, filter_to_apply)
-                    core.save_image(image)
+                    core.save_image(image,path)
                     core.image_nbr += 1
         except NameError:
             print("No entry for a directory or no filters found")
