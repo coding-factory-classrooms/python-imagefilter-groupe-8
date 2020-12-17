b99c38d0838094921dbec3f454f39b78d21cadab
PaulGeneve <geneve.paul1295@gmail.com>
2020-12-17 12:03:33 +0100

    doc logger.py

--- logger.py
+++ logger.py
@@ -3,13 +3,14 @@ from datetime import datetime
 now = datetime.now()
 timestamp = now.strftime('%Y/%m/%d %H:%M:%S')
 timestamp_file = now.strftime('%Y-%m-%d_%H-%M-%S')
+log_file = f'imagefilter-{timestamp_file}.log'.strip()
 
 def log(msg):
-    now = datetime.now()
-    timestamp = now.strftime('%Y/%m/%d %H:%M:%S')
-    timestamp_file = now.strftime('%y-%m-%d_%H-%M-%S')
+    f"""
+    Saves the msg in a log file {log_file} in log directory and prints it in the console.
+    :param msg: the message to be append in the log file
+    """
     formatted = f'{timestamp} - {msg}'
-    log_file = f'imagefilter-{timestamp_file}.log'.strip()
     print(formatted)
     with open(f'log/{log_file}', 'a') as f:
         f.write(formatted + '\n')
