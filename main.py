847432a6d14141961d8b5fd65eaeb076febc27e1
Infernaton <lubin.pauly@gmail.com>
2020-12-17 12:04:57 +0100

    added a dict to stock: the input, the output and the filters

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from logger import *
 try :
     with open('config.txt','r') as f:
         args = f.read().replace('"','').split()
-        print("reading...")
+        print("Reading config.txt")
 except FileNotFoundError:
     args = sys.argv
 
 print(args)
-
+args_dict = {}
 
 try:
     if args[1] == '-h':  # The help message
@@ -25,35 +25,35 @@ try:
     else:
         for i in range(len(args) - 1):
             if args[i] == '-i':  # -i is used to define the directory where the picture is come from
-                entry = f'{args[i + 1]}/'
-                log(f'Directory Input={entry}')
+                args_dict['entry'] = f'{args[i + 1]}/'
+                log(f'Directory Input={args_dict["entry"]}')
 
             elif args[i] == '-o':  # -o is the output where the transformed image will be put
-                output = f'{args[i + 1]}/'
-                log(f'Directory Output={output}')
+                args_dict['output'] = f'{args[i + 1]}/'
+                log(f'Directory Output={args_dict["output"]}')
 
             elif args[i] == '--filters':  # --filters to select the filter for the picture
-                filter_to_apply = args[i + 1].split("|")  # .split(), split the string into a list with the given separator
+                args_dict["filter_to_apply"] = args[i + 1].split("|")  # .split(), split the string into a list with the given separator
 
-                for a in range(len(filter_to_apply)):
+                for a in range(len(args_dict["filter_to_apply"])):
                     # If a blur filter want to be applied, it will change the string to a dict which indicate the blur value
 
-                    if filter_to_apply[a].find("blur:") != -1:
-                        filter_to_apply[a] = core.transform_filter(filter_to_apply[a])
+                    if args_dict["filter_to_apply"][a].find("blur:") != -1:
+                        args_dict["filter_to_apply"][a] = core.transform_filter(args_dict["filter_to_apply"][a])
 
-                    elif filter_to_apply[a].find("dilate:") != -1:
-                        filter_to_apply[a] = core.transform_filter(filter_to_apply[a])
+                    elif args_dict["filter_to_apply"][a].find("dilate:") != -1:
+                        args_dict["filter_to_apply"][a] = core.transform_filter(args_dict["filter_to_apply"][a])
 
-                    elif filter_to_apply[a].find("blur") != -1 or filter_to_apply[a].find("dilate") != -1:
-                        print(f"You need to define a value for the {filter_to_apply[a]} filter")
-                log(f"FILTERS TO USE - {filter_to_apply}")
+                    elif args_dict["filter_to_apply"][a].find("blur") != -1 or args_dict["filter_to_apply"][a].find("dilate") != -1:
+                        print(f"You need to define a value for the {args_dict['filter_to_apply']} filter")
+                log(f"FILTERS TO USE - {args_dict['filter_to_apply']}")
 
         try: # Because 'entry' and 'filter_to_apply' can not be define, there an except
-            with os.scandir(entry) as entries:  # Open the directory as a list
+            with os.scandir(args_dict['entry']) as entries:  # Open the directory as a list
                 for file in entries:  # file represent a picture
-                    path = f'{output}{file.name}'
-                    image = cv2.imread(f'{entry}{file.name}')  # Reference our image
-                    image = core.modify_img(image, filter_to_apply)
+                    path = f'{args_dict["output"]}{file.name}'
+                    image = cv2.imread(f'{args_dict["entry"]}{file.name}')  # Reference our image
+                    image = core.modify_img(image, args_dict["filter_to_apply"])
                     core.save_image(image,path)
                     core.image_nbr += 1
         except NameError:
