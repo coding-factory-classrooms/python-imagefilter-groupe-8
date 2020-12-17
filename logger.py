fed3e3a9d475c3ec6ec985b4e4df2d5e3dd4e802
Infernaton <lubin.pauly@gmail.com>
2020-12-17 09:44:30 +0100

    Log multiple file

--- logger.py
+++ logger.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
-log_file = 'imagefilter.log'
-
 def log(msg):
     now = datetime.now()
     timestamp = now.strftime('%Y/%m/%d %H:%M:%S')
+    timestamp_file = now.strftime('%Y-%m-%d_%H-%M-%S')
     formatted = f'{timestamp} - {msg}'
+    log_file = f'imagefilter-{timestamp_file}.log'.strip()
     print(formatted)
-    with open(log_file, 'a') as f:
+    with open(f'log/{log_file}', 'a') as f:
         f.write(formatted + '\n')
 
-def dump_log():
-    try:
-        with open(log_file, 'r') as f:
-            print(f.read())
-    except FileNotFoundError as e:
-        print(f'Cannot open {log_file}. error={e}')
+# def dump_log():
+#     try:
+#         with open(f'log/{log_file}', 'r') as f:
+#             print(f.read())
+#     except FileNotFoundError as e:
+#         print(f'Cannot open {log_file}. error={e}')
